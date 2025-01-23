3cb6b9154ea480eb7d87630007990e0b02e0955e
PrimePentad <primepentad@gmail.com>
2025-01-23 15:27:46 -0600

    Music for level 1 added
    Changed tolerance for the horizontal difference between the ball and the paddle (needs testing for paddle speed and winnability)
    
    Set minimum velocity for the ball
    
    Player locked out of control for music intro, ball starts in random direction after intro

--- Scripts/B411.gd
+++ Scripts/B411.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 class_name Ball
 
+signal life_start #resets music
 signal life_lost #connected to the death_zone node
 
-const VELOCITY_LIMIT = 40 #The upper bound for speed for the ball
+const VELOCITY_MIN = 10 #The lower bound for the speed of the ball
+const VELOCITY_MAX = 40 #The upper bound for speed of the ball
 
 @export var ball_speed = 20
 @export var lives = 3
@@ -16,6 +18,7 @@ var speed_up_factor = 1.01
 var last_collider_id
 var start_postion: Vector2
 var boost_ready : bool = true
+var player_control : bool = false
 
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -26,6 +29,7 @@ func _ready():
 	start_postion = position
 	death_zone.life_lost.connect(on_life_lost)
 	animated_sprite_2d.play("default")
+	reset_ball()
 
 func _physics_process(delta: float) -> void: #manages all movement of the ball, currently can only be connected to a single kind of brick (BrickBrick) 
 	ui.set_boost(boost_ready)
@@ -45,37 +49,48 @@ func _physics_process(delta: float) -> void: #manages all movement of the ball, 
 
 #attempting to add player input to effect the ball
 func _input(event):
+	if player_control == false:
+		return
 	if Input.is_action_just_pressed("Up") and boost_ready == true:
 		boost_ready = false
 		$Timer.start()
 		animated_sprite_2d.play("Up")
-		velocity = velocity + (Vector2(0,-1) * boost) 
+		velocity = velocity + (Vector2(0,-1) * boost)
+		min_velocity_check()
 	elif Input.is_action_just_pressed("Down") and boost_ready == true:
 		boost_ready = false
 		$Timer.start()
 		animated_sprite_2d.play("Down")
-		velocity = velocity + (Vector2(0,1) * boost) 
+		velocity = velocity + (Vector2(0,1) * boost)
+		min_velocity_check()
 	elif Input.is_action_just_pressed("Right") and boost_ready == true:
 		boost_ready = false
 		$Timer.start()
 		animated_sprite_2d.play("Right")
-		velocity = velocity + (Vector2(1,0) * boost) 
+		velocity = velocity + (Vector2(1,0) * boost)
+		min_velocity_check()
 	elif Input.is_action_just_pressed("Left") and boost_ready == true:
 		boost_ready = false
 		$Timer.start()
 		animated_sprite_2d.play("Left")
-		velocity = velocity + (Vector2(-1,0) * boost) 
+		velocity = velocity + (Vector2(-1,0) * boost)
+		min_velocity_check()
 	else:
 		animated_sprite_2d.play("default")
 		return
 
+#requires ball to move at a minimum velocity
+func min_velocity_check():
+	if velocity.length() < VELOCITY_MIN:
+		velocity = velocity.normalized() * VELOCITY_MIN
+	print(velocity.length())
 
 # randomized the starting direction of the ball. Not sure if this is how we want to start levels but is a simple default 
-func start_ball():
-	position = start_postion
-	randomize()
+#func start_ball():
+	#position = start_postion
+	#randomize()
 	
-	velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1)).normalized() * ball_speed
+	#velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1)).normalized() * ball_speed
 
 func on_life_lost():
 	lives -= 1
@@ -83,13 +98,20 @@ func on_life_lost():
 		ui.game_over()
 	else:
 		life_lost.emit()
-		reset_ball()
 		ui.set_lives(lives)
+		reset_ball()
 
-#after the ball is lost, this is will reset the ball to the spawn position. If we change the default we will need to consider this too
+#ran at level start and after death. resets ball position, resets music, 
+#shoots ball in random direction
 func reset_ball():
+	player_control = false
 	position = start_postion
 	velocity = Vector2.ZERO
+	life_start.emit()
+	await get_tree().create_timer(2.0).timeout
+	velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1)).normalized() * ball_speed
+	player_control = true
+
 
 # This obnoxious bit of code is to create variation in the ball bounces. It speeds up the ball when it hits bricks/paddle and makes the bounce direction
 #- contingent on where the ball hits the paddle or brick. Good to have, stupid to look at. 
@@ -123,8 +145,8 @@ func ball_collision(collision, collider):
 	
 	var speed_multiplier = speed_up_factor if collider is Paddle else 1
 	
-	velocity = (new_velocity * speed_multiplier).limit_length(VELOCITY_LIMIT)
-
+	velocity = (new_velocity * speed_multiplier).limit_length(VELOCITY_MAX)
+	min_velocity_check()
 
 func _on_timer_timeout():
 	boost_ready = true # Replace with function body.
